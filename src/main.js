b9cbf3fbe12d5b40ad9776d70b49e17817ff10d8
xuenenglu <zzz13129180808@gmail.com>
2018-11-01 09:04:09 +0800

    修改CmJS的语法 修改Html错误语法

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import request from './request'
-<<<<<<< HEAD
-import custom from './custom'
-=======
 import hljs from 'hljs'
->>>>>>> 062794ffead6c8f539160a2736ab834fdc68e3e7
 
 Vue.prototype.$request = request
-Vue.use(custom);
+Vue.prototype.dateTrans = function (oDate) {
+    var date = new Date(oDate);
+    let Y = date.getFullYear();
+    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+    let D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    let h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    let m = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+    return (Y + '-' + M + '-' + D + ' ' + h + ':' + m + ':' + s);
+};
 
 Vue.config.productionTip = false
 
